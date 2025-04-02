1b320dd3f30ec6c8b51373dd8c344d7d814f9d42
wmsdsb <3457346173@qq.com>
2025-04-02 17:07:24 +0800

    update

--- exercises/intro/intro1.rs
+++ exercises/intro/intro1.rs
@@ -1,14 +1,5 @@
 // intro1.rs
 //
-// About this `I AM NOT DONE` thing:
-// We sometimes encourage you to keep trying things on a given exercise, even
-// after you already figured it out. If you got everything working and feel
-// ready for the next exercise, remove the `I AM NOT DONE` comment below.
-//
-// If you're running this using `rustlings watch`: The exercise file will be
-// reloaded when you change one of the lines below! Try adding a `println!`
-// line, or try changing what it outputs in your terminal. Try removing a
-// semicolon and see what happens!
 //
 // Execute `rustlings hint intro1` or use the `hint` watch subcommand for a
 // hint.
@@ -23,16 +14,16 @@ fn main() {
     println!(" | |  | |_| \__ \ |_| | | | | | (_| \__ \ ");
     println!(" |_|   \__,_|___/\__|_|_|_| |_|\__, |___/ ");
     println!("                               |___/      ");
-    println!("");
+    println!();
     println!("This exercise compiles successfully. The remaining exercises contain a compiler");
     println!("or logic error. The central concept behind Rustlings is to fix these errors and");
     println!("solve the exercises. Good luck!");
-    println!("");
+    println!();
     println!("The source for this exercise is in `exercises/intro/intro1.rs`. Have a look!");
     println!(
         "Going forward, the source of the exercises will always be in the success/failure output."
     );
-    println!("");
+    println!();
     println!(
         "If you want to use rust-analyzer, Rust's LSP implementation, make sure your editor is set"
     );
