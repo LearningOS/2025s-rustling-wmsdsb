d89b85f665b3966f67665fbc2fbaf464d722a032
wmsdsb <3457346173@qq.com>
2025-04-13 18:50:48 +0800

    update

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -5,7 +5,6 @@
 // Execute `rustlings hint clippy3` or use the `hint` watch subcommand for a hint.
 
 
-
 fn main() {
     let my_option: Option<()> = None;
     if my_option.is_none() {
@@ -18,13 +17,12 @@ fn main() {
     ];
     println!("My array! Here it is: {:?}", my_arr);
 
-    let mut my_empty_vec = vec![1, 2, 3, 4, 5];
-    my_empty_vec.clear();
-    println!("This Vec is empty, see? {:?}", my_empty_vec);
+    let mut my_vec = vec![1, 2, 3, 4, 5];
+    my_vec.clear();  // 使用 resize 清空 vector
+    println!("This Vec is empty, see? {:?}", my_vec);
 
     let mut value_a = 45;
     let mut value_b = 66;
-    // Let's swap these two!
     std::mem::swap(&mut value_a, &mut value_b);
     println!("value a: {}; value b: {}", value_a, value_b);
 }
