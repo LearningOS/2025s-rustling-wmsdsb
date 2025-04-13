d98908a8500aa07d4dd8c76466530ff46fec59ec
wmsdsb <3457346173@qq.com>
2025-04-13 18:47:48 +0800

    update

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -5,11 +5,11 @@
 // Execute `rustlings hint clippy3` or use the `hint` watch subcommand for a hint.
 
 
-#[allow(unused_variables, unused_assignments)]
+
 fn main() {
     let my_option: Option<()> = None;
     if my_option.is_none() {
-        panic!("my_option is none");
+        panic!("Expected a value but got None");
     }
 
     let my_arr = &[
@@ -25,8 +25,6 @@ fn main() {
     let mut value_a = 45;
     let mut value_b = 66;
     // Let's swap these two!
-    // value_a = value_b;
-    // value_b = value_a;
     std::mem::swap(&mut value_a, &mut value_b);
     println!("value a: {}; value b: {}", value_a, value_b);
 }
