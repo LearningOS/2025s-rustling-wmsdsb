9d084089b4c192d1b4eb5b4eeadd9014d8a82b01
wmsdsb <3457346173@qq.com>
2025-04-13 19:57:27 +0800

    update

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -8,9 +8,12 @@
 #[allow(unused_variables, unused_assignments)]
 fn main() {
     let my_option: Option<()> = None;
-    if my_option.is_none() {
-        panic!("None");
+    
+    match my_option{
+        Some(x) => x,
+        None => panic!("None"),
     }
+    
 
     let my_arr = &[
         -1, -2, -3,
@@ -18,13 +21,13 @@ fn main() {
     ];
     println!("My array! Here it is: {:?}", my_arr);
 
-    let mut vec_empty = vec![1, 2, 3, 4, 5];
-    vec_empty.clear();
-    println!("This Vec is empty, see? {:?}", vec_empty);
+    vec![1, 2, 3, 4, 5].resize(0, 5);
+    println!("This Vec is empty, see? {:?}", ());
 
     let mut value_a = 45;
     let mut value_b = 66;
     // Let's swap these two!
+    
     std::mem::swap(&mut value_a, &mut value_b);
     println!("value a: {}; value b: {}", value_a, value_b);
 }
